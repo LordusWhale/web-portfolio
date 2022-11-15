54c41e9c93a55027bd2508827e75b59950039ad0
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-15 21:24:49 +1100

    Added samll navbar

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ document.addEventListener('scroll', (e) => {
     let scroll = window.pageYOffset;
     img.style.backgroundPositionY = scroll * 0.9 + 'px';
 })
+
+const navbar = document.getElementById('nav')
+const smallNav = document.getElementById('small-nav-id');
+const smallNavObser = new IntersectionObserver(entries=>{
+    entries.forEach(navBar => {
+        smallNav.classList.toggle('in-view', !navBar.isIntersecting)
+    })
+})
+
+smallNavObser.observe(navbar);
