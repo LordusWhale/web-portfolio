f79880620f1028113c4c4c3468a53d7ebdfc6881
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-15 21:11:57 +1100

    Added projects page

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const navBarObserver = new IntersectionObserver(entries=>{
+const observer = new IntersectionObserver(entries=>{
     entries.forEach(entry => {
         if (entry.isIntersecting){
             entry.target.classList.add('in-view')
         }
     })
+}, {
+    threshold: 0.8
 })
 
-const navBarElements = document.querySelectorAll('.observe');
-let transitionDelay = 200;
-navBarElements.forEach(el=>{
-    el.style.transitionDelay = `${transitionDelay}ms` 
-    navBarObserver.observe(el)
-    transitionDelay += 100;
+const elements = document.querySelectorAll('.observe');
+elements.forEach(el=>{
+    observer.observe(el)
+})
+const img = document.getElementById('home')
+document.addEventListener('scroll', (e) => {
+    let scroll = window.pageYOffset;
+    img.style.backgroundPositionY = scroll * 0.9 + 'px';
 })
